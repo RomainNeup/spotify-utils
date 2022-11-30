d878695174a57f3d1bca432bb0f237f810d519de
Romain <romain.neuplanche@epitech.eu>
2022-12-01 00:42:47 +0100

    OnePage app

--- src/lib/elements/authorization/Callback.svelte
+++ src/lib/elements/authorization/Callback.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
     import { accessToken } from "../../../store/spotify";
 
-
-    if (window.location.pathname === "/callback") {
+    if (window.location.hash) {
         const params = new URLSearchParams(window.location.hash.slice(1));
         accessToken.set(params.get("access_token"));
         window.history.pushState("", "", "/");
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
   plugins: [svelte()],
-  base: mode === "production" ? 'spotify-utils' : '',
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        nested: '404.html'
-      }
-    }
-  }
+  base: mode === "production" ? 'spotify-utils' : ''
 }));

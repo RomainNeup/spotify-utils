d9fa59677260f22c85e90c45389335489c35e38d
Romain <romain.neuplanche@epitech.eu>
2022-11-30 03:06:23 +0100

    Change baseURL

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+export default defineConfig(({mode}) => ({
+  plugins: [svelte()],
+  base: mode === "production" ? 'spotify-utils' : ''
+}));
